ce35dee1d9fe1ac8dcc769b4c3b07450aaeb9f84
Berin Loritsch <berin@d-haven.org>
2017-06-22 19:49:51 -0400

    basic scratchpad of a raw cut for some structure.  Nothing really works

--- DHaven.Faux/Compiler/DiscoveryAwareBase.cs
+++ DHaven.Faux/Compiler/DiscoveryAwareBase.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Steeltoe.Discovery;
 using Steeltoe.Discovery.Client;
 
@@ -11,19 +13,29 @@ namespace DHaven.Faux.Compiler
     public class DiscoveryAwareBase
     {
         private readonly DiscoveryHttpClientHandler handler;
+        private readonly Uri baseUri = new Uri("http://serviceName/api/base");
 
         public DiscoveryAwareBase(IDiscoveryClient client)
         {
             handler = new DiscoveryHttpClientHandler(client);
         }
 
-        protected async Task<String> GetString(string url)
+        protected async Task<HttpResponseMessage> SendAsJsonAsync<TObject>(HttpMethod method, string endPoint, TObject data)
         {
-            var client = GetClient();
-            return await client.GetStringAsync(url);
+            using (var client = GetClient())
+            {
+                var requestMessage = new HttpRequestMessage(method, GetServiceUri(endPoint));
+
+                return await client.SendAsync(requestMessage);
+            }
         }
 
-        protected HttpClient GetClient()
+        private Uri GetServiceUri(string endPoint)
+        {
+            return new Uri(baseUri, endPoint);
+        }
+
+        private HttpClient GetClient()
         {
             return new HttpClient(handler, false);
         }
