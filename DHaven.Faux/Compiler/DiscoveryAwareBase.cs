8e8bdc3cf37529abb33ff070cd0d52d41fec3fec
Berin Loritsch <berin@d-haven.org>
2017-06-21 20:25:08 -0400

    Add project files.

---
+++ DHaven.Faux/Compiler/DiscoveryAwareBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Steeltoe.Discovery;
+using Steeltoe.Discovery.Client;
+
+namespace DHaven.Faux.Compiler
+{
+    public class DiscoveryAwareBase
+    {
+        private readonly DiscoveryHttpClientHandler handler;
+
+        public DiscoveryAwareBase(IDiscoveryClient client)
+        {
+            handler = new DiscoveryHttpClientHandler(client);
+        }
+
+        protected async Task<String> GetString(string url)
+        {
+            var client = GetClient();
+            return await client.GetStringAsync(url);
+        }
+
+        protected HttpClient GetClient()
+        {
+            return new HttpClient(handler, false);
+        }
+    }
+}
