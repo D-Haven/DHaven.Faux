66ae824fed471aefcd608572781c5dced455e047
Berin Loritsch <bloritsch@d-haven.org>
2017-06-24 13:17:49 -0400

    Formatting, make the base SendAsJson method work with json content.

--- DHaven.Faux/Compiler/DiscoveryAwareBase.cs
+++ DHaven.Faux/Compiler/DiscoveryAwareBase.cs
@@ -1,32 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
+﻿#region Copyright 2017 D-Haven.org
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Steeltoe.Discovery;
+using Newtonsoft.Json;
 using Steeltoe.Discovery.Client;
 
 namespace DHaven.Faux.Compiler
 {
     public class DiscoveryAwareBase
     {
-        private readonly DiscoveryHttpClientHandler handler;
         private readonly Uri baseUri = new Uri("http://serviceName/api/base");
+        private readonly DiscoveryHttpClientHandler handler;
 
         public DiscoveryAwareBase(IDiscoveryClient client)
         {
             handler = new DiscoveryHttpClientHandler(client);
         }
 
-        protected async Task<HttpResponseMessage> SendAsJsonAsync<TObject>(HttpMethod method, string endPoint, TObject data)
+        protected async Task<TResponse> SendAsJsonAsync<TRequest, TResponse>(
+            HttpMethod method, string endPoint, TRequest data)
         {
             using (var client = GetClient())
             {
                 var requestMessage = new HttpRequestMessage(method, GetServiceUri(endPoint));
 
-                return await client.SendAsync(requestMessage);
+                if (data != null)
+                {
+                    var json = JsonConvert.SerializeObject(data);
+                    requestMessage.Content = new StringContent(json, Encoding.UTF8, "application/json");
+                }
+
+                var responseMessage = await client.SendAsync(requestMessage);
+
+                if (responseMessage.StatusCode == HttpStatusCode.NoContent)
+                {
+                    return default(TResponse);
+                }
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    return JsonConvert.DeserializeObject<TResponse>(await responseMessage.Content.ReadAsStringAsync());
+                }
+
+                throw new HttpRequestException(
+                    $"Unsuccessful response status: {responseMessage.StatusCode} {responseMessage.ReasonPhrase}");
             }
         }
 
--- DHaven.Faux/Compiler/WebServiceCompiler.cs
+++ DHaven.Faux/Compiler/WebServiceCompiler.cs
@@ -1,14 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿#region Copyright 2017 D-Haven.org
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
 using System.Reflection;
-using System.Text;
 
 namespace DHaven.Faux.Compiler
 {
-    internal class WebServiceCompiler<TService> where TService : class
+    internal class WebServiceComplier
     {
-        private readonly TypeInfo typeInfo = typeof(TService).GetTypeInfo();
+        private readonly TypeInfo typeInfo;
+
+        protected WebServiceComplier(TypeInfo type)
+        {
+            typeInfo = type;
+        }
 
         public void Verify()
         {
@@ -18,9 +36,20 @@ namespace DHaven.Faux.Compiler
             }
         }
 
-        public TService Generate()
+        public object Generate()
         {
             throw new NotImplementedException();
+        }
+    }
+
+    internal class WebServiceCompiler<TService> : WebServiceComplier
+        where TService : class
+    {
+        public WebServiceCompiler() : base(typeof(TService).GetTypeInfo()) { }
+
+        public new TService Generate()
+        {
+            return base.Generate() as TService;
         }
     }
 }
